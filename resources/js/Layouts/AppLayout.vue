d1df015b4fe31a92acccbe36716444bc4719eb6e
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-03-14 19:31:29 +0600

    error fix

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -322,7 +322,7 @@ const userMenu = ref(false);
     </div>
 </aside>
 
-<div class="p-4 sm:ml-64">
+<div class="p-4 sm:ml-64 pt-[78px]">
     <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
         <slot />
     </div>
