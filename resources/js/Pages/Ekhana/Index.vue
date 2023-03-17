6079e4b66428542eb095a5d1288f5bdb88063e1c
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-03-17 20:15:19 +0600

    money formate

--- resources/js/Pages/Ekhana/Index.vue
+++ resources/js/Pages/Ekhana/Index.vue
@@ -185,7 +185,7 @@ const ekhanaFetch = ()=>{
                     <tr v-for="(value, key) in ekhana"
                         class="bg-white text-center border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{ value.holding_no }}
+                            {{ value.holding_no  }}
                         </th>
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ value.name }}
--- resources/js/Pages/Setup/Tax/Index.vue
+++ resources/js/Pages/Setup/Tax/Index.vue
@@ -41,15 +41,6 @@ function DateFormate(date){
     }
 return date;
 }
-// Create our number formatter.
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'BDT',
-
-  // These options are needed to round to whole numbers if that's what you want.
-  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-});
 </script>
 
 <template>
@@ -101,7 +92,7 @@ const formatter = new Intl.NumberFormat('en-US', {
                                     </td>
                                     <td scope="row"
                                         class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {{ formatter.format(value.price) }}
+                                        {{ new Intl.NumberFormat().format(value.price) }} /=
                                     </td>
                                     <td v-text="DateFormate(value.created_at) " class="px-6 py-4">
 
