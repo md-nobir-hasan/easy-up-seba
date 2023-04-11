600e673890e82d4f48a385f2929916d26b2a9525
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-04-11 21:59:35 +0600

    Update Index.vue

--- resources/js/Pages/Tax/Calculation/VillageLevy/Index.vue
+++ resources/js/Pages/Tax/Calculation/VillageLevy/Index.vue
@@ -280,7 +280,7 @@ const exportExcel = ()=>{
                         <th rowspan="2">অর্থ-বছর</th>
                         <th rowspan="2">হাল করের পরিমাণ</th>
                         <th rowspan="2">বকেয়া করের পরিমাণ</th>
-                        <th rowspan="2">সরোট করের পরিমাণ</th>
+                        <th rowspan="2">সর্বমোট করের পরিমাণ</th>
                         <!-- <th rowspan="2">স্বাক্ষর</th> -->
                       </tr>
                       <tr>
